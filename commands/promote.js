78617f2dfeb196728098f7b9b5034c600d1fc867
FHGDev <gideon.lingle14@gmail.com>
2018-08-06 23:46:06 -0500

    Update promote.js

--- commands/promote.js
+++ commands/promote.js
@@ -2,7 +2,7 @@ const discord = require('discord.js')
 const roblox = require('roblox-js')
 const groupid = 4173965
 module.exports.run = (bot, message, args, em) => {
-  const name = args[1]
+  const name = args[0]
   const role = args.slice(1).join(' ')
   roblox.login('FreakingHulk', process.env.pw)
   roblox.getIdFromUsername(name).then(id => {
