ea1436cd1c4ff45d8473f915937ddcd3c2f3872d
FHGDev <gideon.lingle14@gmail.com>
2018-08-21 12:31:25 -0400

    Update promote.js

--- commands/promote.js
+++ commands/promote.js
@@ -3,11 +3,11 @@ const roblox = require('roblox-js')
 const groupid = 4173965
 module.exports.run = (bot, message, args, em) => {
   const name = args[1]
-  const role = args[2]
+  const role = args.join(' ').slice(0,1)
   roblox.login('FreakingHulk', process.env.pw)
   roblox.getIdFromUsername(name).then(id => {
     roblox.setRank(groupid, id, role).then(() => {
-      console.log(args)
+      console.log(args.join(" "));
       message.channel.send(`Promoted ${name}.`)
     }).catch(err => {
       console.error(err)
